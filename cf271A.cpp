8f7de96ec6c3266e258b25e4784f89e7419a721e
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-02-19 20:11:27 +0600

    Add files via upload

---
+++ cf271A.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+
+    for(; ; )
+    {
+        a++;
+        int b=(a/1000);
+        int c=(a/100)%10;
+        int d=(a/10)%10;
+        int e=a%10;
+
+        if(b!=c && c!=d && d!=e && e!=b && b!=d && c!=e )
+        {
+            break;
+        }
+    }
+    cout<<a<<endl;
+
+
+
+}
+
+
+
+
+
+
