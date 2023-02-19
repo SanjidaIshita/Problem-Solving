3bb55a7b04850ee62d9e45dfda6fd08dcc30b932
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-02-19 16:25:53 +0600

    Add files via upload

---
+++ cf41A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    reverse(a.begin(),a.end());
+    if(a==b)
+    {
+        cout<<"YES"<<endl;
+
+
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+
+
+
+
