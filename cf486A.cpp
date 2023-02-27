f9f4dcbc494a498b056cc49dc3cc29d31418c377
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-02-27 07:23:41 +0600

    Add files via upload

---
+++ cf486A.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int k;
+    cin>>k;
+
+        if(k%2==0)
+        {
+            cout<<k/2;
+        }
+        else
+        {
+            cout<<'-'<<k/2+1<<endl;;
+        }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
