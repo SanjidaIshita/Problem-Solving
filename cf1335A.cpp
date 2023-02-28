ce0aa3831387ed9c2de7e9ffe72ab0dabbd41a44
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-03-01 00:03:04 +0600

    Add files via upload

---
+++ cf1335A.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b,x;
+    cin>>n;
+
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>x;
+        if(x%2==0)
+        {
+            cout<<x/2-1<<endl;
+
+
+        }
+        else
+        {
+            cout<<x/2<<endl;
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
