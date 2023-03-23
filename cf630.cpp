74c61008b6bc47460ce4f16fe294406cd7744b10
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-03-23 22:45:09 +0600

    Add files via upload

---
+++ cf630.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t;
+    cin>>t;
+    long long int result = pow(5, t);
+
+// printing the result upto 2
+// decimal place
+cout << fixed << setprecision(0) << 25 << endl;
+}
+
