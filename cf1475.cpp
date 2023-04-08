8c5e052921b7000ac508ce2832805da101bbe2e5
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-04-08 23:53:21 +0600

    Add files via upload

---
+++ cf1475.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+
+
+    for(int i=0; i<t; i++)
+    {
+        long long int a,x;
+         cin>>a;
+
+
+
+
+           if((a&(a-1))==0)
+           {
+               cout<<"NO"<<endl;
+           }
+           else
+           {
+               cout<<"Yes"<<endl;
+           }
+
+
+
+}
+}
+
