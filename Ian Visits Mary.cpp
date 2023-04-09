9e55b45cd66d649bf6a6e9956016682df64bb9f3
Sanjida Ishita <42861758+SanjidaIshita@users.noreply.github.com>
2023-04-10 00:41:35 +0600

    Add files via upload

---
+++ Ian Visits Mary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+
+    for(int i=0; i<t; i++)
+    {
+        long long int x,y;
+        cin>>x>>y;
+       cout<< "2" <<endl;
+       cout<<x-1<<" "<<1<<endl;
+       cout<<x<<" "<<y<<endl;
+    }
+}
+
